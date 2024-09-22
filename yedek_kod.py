0bd2fd7578fbc6e7428178952c0e38a56ff8bdb7
emirrxw01 <emirbozkurt698@gmail.com>
2024-09-22 21:47:31 +0300

    yedek_kod.py

--- yedek_kod.py
+++ yedek_kod.py
@@ -12,7 +12,7 @@ def generate_backup_codes(length):
 def brute_force_backup_codes(username, password, backup_codes):
     url = 'https://www.instagram.com/accounts/login/ajax/'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) C>
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         'X-CSRFToken': '',
         'Content-Type': 'application/x-www-form-urlencoded'
     }
